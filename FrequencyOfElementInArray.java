31d91d07d4a84a326187b9d523b8eb654f6480a4
pv-tech28 <prathavarshney10@gmail.com>
2025-09-08 14:59:32 +0530

    Update FrequencyOfElement.java

--- FrequencyOfElementInArray.java
+++ FrequencyOfElementInArray.java
@@ -20,7 +20,8 @@ public class FrequencyOfElementInArray {
                 freq++;
             }
         }
-        System.out.println("Frequency of" + e +  "is: "+ freq);
+        System.out.println("Frequency of " +e+  " is: "+ freq);
     }
 }
+
 
